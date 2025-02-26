e061c49ff5def4a65cccd839172be9a07929015d
Deepa131 <deepapaudel65@gmail.com>
2025-02-27 01:25:12 +0545

    some changes

--- routes/diaryRoutes.js
+++ routes/diaryRoutes.js
@@ -5,9 +5,13 @@ const diaryController = require('../controllers/diaryController');
 const router = express.Router();
 
 router.post('/create', authMiddleware.verifyToken, diaryController.createDiaryEntry);
+
 router.get('/all', authMiddleware.verifyToken, diaryController.getAllDiaryEntries);
+
 router.get('/:id', authMiddleware.verifyToken, diaryController.getDiaryEntryById);
+
 router.put('/:id', authMiddleware.verifyToken, diaryController.updateDiaryEntry);
+
 router.delete('/:id', authMiddleware.verifyToken, diaryController.deleteDiaryEntry);
 
 module.exports = router;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,11 +5,14 @@ const authMiddleware = require('../middleware/authMiddleware');
 
 // User Authentication Routes
 router.post('/signup', userController.signupUser);
+
 router.post('/login', userController.loginUser);
 
 // Protected User Routes (Require Authentication)
-router.get('/profile', authMiddleware.verifyToken, userController.getUserById);  
-router.put('/profile', authMiddleware.verifyToken, userController.updateUser);   
-router.delete('/profile', authMiddleware.verifyToken, userController.deleteUser); 
+router.get('/profile', authMiddleware.verifyToken, userController.getUserById);
+
+router.put('/profile', authMiddleware.verifyToken, userController.updateUser);
+
+router.delete('/profile', authMiddleware.verifyToken, userController.deleteUser);
 
 module.exports = router;
